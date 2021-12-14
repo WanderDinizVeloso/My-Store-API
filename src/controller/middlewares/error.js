d39ba9d16d983ec48ca903909cc8f781047a412a
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-14 14:48:24 -0300

    corrige instalacao do http-status-codes e corrige desistruturacao do middleware error

--- src/controller/middlewares/error.js
+++ src/controller/middlewares/error.js
@@ -1,4 +1,4 @@
-const { StatusCodes } = require('http-status-code');
+const { INTERNAL_SERVER_ERROR } = require('http-status-codes').StatusCodes;
 
 module.exports = async (err, _req, res, _next) => {
   const { status = null, message } = err;
@@ -10,6 +10,6 @@ module.exports = async (err, _req, res, _next) => {
   }
 
   return res
-    .status(StatusCodes.INTERNAL_SERVER_ERROR)
+    .status(INTERNAL_SERVER_ERROR)
     .json({ error: { message: 'sorry, internal error.' } });
 };
