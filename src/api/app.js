ea012312af880cce5bb430ded504305843f0fb3b
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-14 14:18:47 -0300

    cria e implementa root no app.js

--- src/api/app.js
+++ src/api/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const { json } = require('body-parser');
 
+const root = require('../controller/routers/root');
+
 const app = express();
 
 app.use(json());
+
+app.use(root);
 
 module.exports = app;
