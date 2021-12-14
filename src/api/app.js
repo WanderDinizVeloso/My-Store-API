3a6674260a01c2632d5b3a24419940e2e50b6eca
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-14 15:00:08 -0300

    instala e implementa dependencia dotenv

--- src/api/app.js
+++ src/api/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { json } = require('body-parser');
 
+require('dotenv').config();
+
 const root = require('../controller/routers/root');
 const { error } = require('../controller/middlewares');
 
