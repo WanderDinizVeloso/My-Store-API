6c77ab6f1f63617a61c911d33d4e6ef62150fe12
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-15 09:41:39 -0300

    cria controller users create.js

---
+++ src/controller/documents/create.js
@@ -0,0 +1,20 @@
+const { CREATED } = require('http-status-codes').StatusCodes;
+
+const { create } = require('../../service/documents/users');
+
+const MESSAGE = 'user created successfully.';
+
+module.exports = async (req, res, _next) => {
+  const { firstName, lastName, email, password } = req.body;
+
+  const newUser = { firstName, lastName, email, password };
+
+  const created = await create(newUser);
+
+  return res
+    .status(CREATED)
+    .json({
+      message: MESSAGE,
+      createdUser: { created },
+    });
+};
