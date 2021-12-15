f657c3b78d6e42f0fdece415001733cce077f6d7
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-15 10:08:00 -0300

    cria controller users remove.js

---
+++ src/controller/documents/remove.js
@@ -0,0 +1,26 @@
+const { OK, NOT_FOUND } = require('http-status-codes').StatusCodes;
+
+const { remove } = require('../../service/documents/users');
+
+const MESSAGE_OK = 'user deleted successfully';
+const MESSAGE_NOT_FOUND = 'user not found';
+
+module.exports = async (req, res, next) => {
+  const { id } = req.params;
+
+  const removed = await remove(id);
+
+  if (!removed) {
+    return next({
+      status: NOT_FOUND,
+      message: MESSAGE_NOT_FOUND,
+    });
+  }
+
+  return res
+    .status(OK)
+    .json({
+      message: MESSAGE_OK,
+      deletedUser: { removed },
+    });
+};
