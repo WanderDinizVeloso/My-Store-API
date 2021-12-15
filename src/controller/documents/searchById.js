c83ad5b16c2bc4d2a6e7dd3b98786573d2e731db
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-15 10:26:05 -0300

    cria controller users searchById.js

---
+++ src/controller/documents/searchById.js
@@ -0,0 +1,22 @@
+const { OK, NOT_FOUND } = require('http-status-codes').StatusCodes;
+
+const { searchById } = require('../../service/documents/users');
+
+const MESSAGE_NOT_FOUND = 'user not found';
+
+module.exports = async (req, res, next) => {
+  const { id } = req.params;
+
+  const user = await searchById(id);
+
+  if (!user) {
+    return next({
+      status: NOT_FOUND,
+      message: MESSAGE_NOT_FOUND,
+    });
+  }
+
+  return res
+    .status(OK)
+    .json({ user });
+};
