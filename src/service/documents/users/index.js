68ecd342321c63f3bc0c8305dc98ccc6268507af
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-14 22:54:02 -0300

    cria service users create.js

--- src/service/documents/users/index.js
+++ src/service/documents/users/index.js
@@ -1,7 +1,9 @@
+const create = require('./create');
 const searchAll = require('./searchAll');
 const searchById = require('./searchById');
 
 module.exports = {
+  create,
   searchAll,
   searchById,
 };
