ab58eb65a868d87e41342b8ea2e7a9b393bc87b5
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-15 16:09:28 -0300

    cria validation verifyPassword.js

---
+++ src/service/validations/verifyPassword.js
@@ -0,0 +1,13 @@
+const LENGTH = 8;
+
+const MESSAGE_NO_LENGTH = 'no length';
+
+module.exports = (password) => {
+  if (!password || password === '') {
+    return null;
+  }
+
+  if (password.length < LENGTH) {
+    return MESSAGE_NO_LENGTH;
+  }
+};
