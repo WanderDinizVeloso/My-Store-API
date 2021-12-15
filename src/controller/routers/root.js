199e957fcbdc0e90846b215716cca6f1637ddf96
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-15 10:50:46 -0300

    cria e implementa router users

--- src/controller/routers/root.js
+++ src/controller/routers/root.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+const users = require('./users');
+
 const root = express.Router({ mergeParams: true });
+
+root.use('/users', users);
 
 module.exports = root;
