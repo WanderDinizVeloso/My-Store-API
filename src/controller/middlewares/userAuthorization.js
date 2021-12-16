52a757109859b4977f1bffc6e951a0be6c34ec0c
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-16 14:35:35 -0300

    cria middleware userAuthorization.js

---
+++ src/controller/middlewares/userAuthorization.js
@@ -0,0 +1,18 @@
+const { UNAUTHORIZED } = require('http-status-codes').StatusCodes;
+
+const { notAuthorization } = require('../../service/utils/messages');
+
+const ROLE_ADM = 'adm';
+
+module.exports = async (req, _res, next) => {
+  const { id } = req.params;
+  const { id: idAuth, role } = req.user;  
+
+  if (id !== idAuth && role !== ROLE_ADM) {
+    return next({
+      status: UNAUTHORIZED, message: notAuthorization(),
+    });
+  }
+
+  return next();
+};
