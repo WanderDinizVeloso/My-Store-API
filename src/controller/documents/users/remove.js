6953d355af6bfecaaa0a4be4b1492a10bf120d36
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-16 14:46:00 -0300

    refatora users remove para remover logica de authorizacao tendo em vista criacao de middleware authorization

--- src/controller/documents/users/remove.js
+++ src/controller/documents/users/remove.js
@@ -1,26 +1,18 @@
-const { OK, NOT_FOUND, UNAUTHORIZED } = require('http-status-codes').StatusCodes;
+const { OK, NOT_FOUND } = require('http-status-codes').StatusCodes;
 
 const { remove } = require('../../../service/documents/users');
 const { deletedSuccessfully, notFound } = require('../../../service/utils/messages');
-const { notAuthorization } = require('../../../service/utils/messages');
 
 const USER = 'user';
 
 module.exports = async (req, res, next) => {
   const { id } = req.params;
-  const { email, role } = req.user;
 
-  const removed = await remove({ id, email, role });
+  const removed = await remove(id);
 
   if (!removed) {
     return next({
       status: NOT_FOUND, message: notFound(USER),
-    });
-  }
-
-  if (removed === notAuthorization()) {
-    return next({
-      status: UNAUTHORIZED, message: notAuthorization(),
     });
   }
 
--- src/service/documents/users/remove.js
+++ src/service/documents/users/remove.js
@@ -1,19 +1,12 @@
 const { remove } = require('../../../model')('users');
 
 const searchByid = require('./searchById');
-const { notAuthorization } = require('../../utils/messages');
 
-const ROLE_ADM = 'adm';
-
-module.exports = async ({ id, email, role }) => {
+module.exports = async (id) => {
   const user = await searchByid(id);
 
   if (!user) {
     return null;
-  }
-
-  if (user.email !== email && role !== ROLE_ADM) {
-    return notAuthorization();
   }
 
   const { deletedCount } = await remove(id);
