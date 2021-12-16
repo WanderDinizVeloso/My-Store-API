f9f100b1784fad49274573714b85f5be5c6c3e9c
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-15 23:25:43 -0300

    cria service utils messages

---
+++ src/service/utils/messages.js
@@ -0,0 +1,34 @@
+const required = (param) =>
+  `The "${param}" field is required.`;
+
+const invalid = (param) =>
+  `The invalid "${param}" field.`;
+
+const notLength = (param, length) =>
+  `The "${param}" field must contain at least ${length} characters`;
+
+const createdSuccessfully = (param) => 
+  `${param} created successfully.`;
+
+const deletedSuccessfully = (param) =>
+  `${param} deleted successfully,`;
+
+const modifiedSuccessfully = (param) =>
+  `${param} modified successfully.`;
+
+const notFound = (param) =>
+  `${param} not found.`;
+
+const notRegistered = (param) =>
+  `no registered ${param}.`;
+
+module.exports = {
+  required,
+  invalid,
+  notLength,
+  createdSuccessfully,
+  deletedSuccessfully,
+  modifiedSuccessfully,
+  notFound,
+  notRegistered,
+};
