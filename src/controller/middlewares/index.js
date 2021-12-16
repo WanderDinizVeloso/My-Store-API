25b80aaff29ebbc463de70119fd617d7903decc0
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-16 00:36:50 -0300

    implementa middleware de validacao no index.js

--- src/controller/middlewares/index.js
+++ src/controller/middlewares/index.js
@@ -1,7 +1,17 @@
 const error = require('./error');
 const wrapper = require('./wrapper');
+const validateEmail = require('./validateEmail');
+const validateFirstName = require('./validateFirstName');
+const validateLastName = require('./validateLastName');
+const validateId = require('./validateId');
+const validatePassword = require('./validatePassword');
 
 module.exports = {
   error,
   wrapper,
+  validateEmail,
+  validateFirstName,
+  validateLastName,
+  validateId,
+  validatePassword,
 };
