08df2dd9f9fc85cb5d8cd5b298c7ae7cb4195ad2
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-16 00:13:09 -0300

    cria middleware validatePassword.js

---
+++ src/controller/middlewares/validatePassword.js
@@ -0,0 +1,31 @@
+const { BAD_REQUEST } = require('http-status-codes').StatusCodes;
+
+const { verifyRequeriment } = require('../../service/validations');
+
+const { required, notLength } = require('../../service/utils/messages');
+
+const PASSWORD = 'password';
+const lENGTH = 10;
+const MESSAGE_NO_LENGTH = 'no length';
+
+module.exports = async (req, _res, next) => {
+  const { lastName } = req.body;
+
+  const validation = verifyRequeriment(lastName, lENGTH);
+
+  if (!validation) {
+    return next({
+      status: BAD_REQUEST,
+      message: required(PASSWORD),
+    });
+  }
+
+  if (validation === MESSAGE_NO_LENGTH) {
+    return next({
+      status: BAD_REQUEST,
+      message: notLength(PASSWORD, lENGTH),
+    });
+  }
+
+  return next();
+};
