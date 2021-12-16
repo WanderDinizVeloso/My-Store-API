ffc1dfe1265fb7ff2a0fd549239592c3127c6cda
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-16 16:47:04 -0300

    cria service products update.js

---
+++ src/service/documents/products/update.js
@@ -0,0 +1,26 @@
+const { update } = require('../../../model')('products');
+
+const searchById = require('./searchById');
+
+module.exports = async (dataProduct) => {
+  const { id, dataProductWithoutId } = dataProduct;
+
+  const product = await searchById(id);
+
+  if (!product) {
+    return null;
+  }
+
+  const modifiedProduct = {
+    ...product,
+    ...dataProductWithoutId,
+  };
+
+  const { modifiedCount } = await update(modifiedProduct);
+
+  const newProductData = await searchById(id);
+
+  const updated = { modifiedCount, newProductData };
+
+  return updated;
+};
