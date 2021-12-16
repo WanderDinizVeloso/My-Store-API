a6c432799bd62865b83524daab62e8be7dd92398
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-16 10:36:12 -0300

    refatora service login

--- src/service/documents/login/login.js
+++ src/service/documents/login/login.js
@@ -3,23 +3,16 @@ const { compare } = require('bcrypt');
 const { searchAll } = require('../../../model')('users');
 
 const { getToken } = require('../../auth');
-const { invalid } = require('../../utils/messages');
-
-const EMAIL_OR_PASSWORD = 'email or password';
 
 module.exports = async ({ email, password }) => {
   const allUsers = await searchAll();
-
-  if (!allUsers.length) {
-    return null;
-  }
 
   const searchUser = allUsers.find((user) => user.email === email);
 
   const matchPassword = await compare(password, searchUser.password);
 
-  if (!searchUser.length || !matchPassword) {
-    return invalid(EMAIL_OR_PASSWORD);
+  if (!allUsers.length || !searchUser.length || !matchPassword) {
+    return null;
   }
 
   const { password: pass, ...userWithoutPassword } = searchUser;
