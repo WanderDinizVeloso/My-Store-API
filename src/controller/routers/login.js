b16bde9b5169c1252b70453ec39f89efde2577b5
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-16 10:42:44 -0300

    cria e implementa router login

---
+++ src/controller/routers/login.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const {
+  wrapper,
+  validateEmail,
+  validatePassword,
+} = require('../middlewares');
+
+const { login } = require('../documents/login');
+
+const router = express.Router({ mergeParams: true });
+
+router.post('/',
+  wrapper(validateEmail), 
+  wrapper(validatePassword),
+  wrapper(login));
+
+module.exports = router;
--- src/controller/routers/root.js
+++ src/controller/routers/root.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const users = require('./users');
+const login = require('./login');
 
 const root = express.Router({ mergeParams: true });
 
 root.use('/users', users);
+root.use('.login', login);
 
 module.exports = root;
