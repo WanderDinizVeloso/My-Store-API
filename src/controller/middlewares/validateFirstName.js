19afea2b96ce613d5dcecda043f6b5c272203b76
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-16 00:03:55 -0300

    cria middleware validateFirstName.js

---
+++ src/controller/middlewares/validateFirstName.js
@@ -0,0 +1,31 @@
+const { BAD_REQUEST } = require('http-status-codes').StatusCodes;
+
+const { verifyRequeriment } = require('../../service/validations');
+
+const { required, notLength } = require('../../service/utils/messages');
+
+const FIRST_NAME = 'firstName';
+const lENGTH = 3;
+const MESSAGE_NO_LENGTH = 'no length';
+
+module.exports = async (req, _res, next) => {
+  const { firstName } = req.body;
+
+  const validation = verifyRequeriment(firstName, lENGTH);
+
+  if (!validation) {
+    return next({
+      status: BAD_REQUEST,
+      message: required(FIRST_NAME),
+    });
+  }
+
+  if (validation === MESSAGE_NO_LENGTH) {
+    return next({
+      status: BAD_REQUEST,
+      message: notLength(FIRST_NAME, lENGTH),
+    });
+  }
+
+  return next();
+};
