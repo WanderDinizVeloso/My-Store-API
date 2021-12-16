a33e683af97350beafaa05d1cd48b001ac0fed99
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-16 00:32:58 -0300

    cria middleware validateId.js

---
+++ src/controller/middlewares/validateId.js
@@ -0,0 +1,31 @@
+const { BAD_REQUEST } = require('http-status-codes').StatusCodes;
+
+const { verifyRequeriment } = require('../../service/validations');
+
+const { required, notLength } = require('../../service/utils/messages');
+
+const ID = 'id';
+const lENGTH = 24;
+const MESSAGE_NO_LENGTH = 'no length';
+
+module.exports = async (req, _res, next) => {
+  const { id } = req.params;
+
+  const validation = verifyRequeriment(id, lENGTH);
+
+  if (!validation) {
+    return next({
+      status: BAD_REQUEST,
+      message: required(ID),
+    });
+  }
+
+  if (validation === MESSAGE_NO_LENGTH) {
+    return next({
+      status: BAD_REQUEST,
+      message: notLength(ID, lENGTH),
+    });
+  }
+
+  return next();
+};
