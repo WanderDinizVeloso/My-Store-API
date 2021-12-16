3b0b9ec623ba4a36c6b7243ffaf50f82511cdcf6
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-16 13:43:39 -0300

    implementa controle de autorizacao no users remove

--- src/controller/documents/users/remove.js
+++ src/controller/documents/users/remove.js
@@ -1,26 +1,32 @@
-const { OK, NOT_FOUND } = require('http-status-codes').StatusCodes;
+const { OK, NOT_FOUND, UNAUTHORIZED } = require('http-status-codes').StatusCodes;
 
 const { remove } = require('../../../service/documents/users');
 const { deletedSuccessfully, notFound } = require('../../../service/utils/messages');
+const { notAuthorization } = require('../../../service/utils/messages');
 
 const USER = 'user';
 
 module.exports = async (req, res, next) => {
   const { id } = req.params;
+  const { email, role } = req.user;
 
-  const removed = await remove(id);
+  const removed = await remove({ id, email, role });
 
   if (!removed) {
     return next({
-      status: NOT_FOUND,
-      message: notFound(USER),
+      status: NOT_FOUND, message: notFound(USER),
+    });
+  }
+
+  if (removed === notAuthorization()) {
+    return next({
+      status: UNAUTHORIZED, message: notAuthorization(),
     });
   }
 
   return res
     .status(OK)
     .json({
-      message: deletedSuccessfully(USER),
-      deletedUser: removed,
+      message: deletedSuccessfully(USER), deletedUser: removed,
     });
 };
--- src/service/documents/users/remove.js
+++ src/service/documents/users/remove.js
@@ -1,12 +1,19 @@
 const { remove } = require('../../../model')('users');
 
 const searchByid = require('./searchById');
+const { notAuthorization } = require('../../utils/messages');
 
-module.exports = async (id) => {
+const ROLE_ADM = 'adm';
+
+module.exports = async ({ id, email, role }) => {
   const user = await searchByid(id);
 
   if (!user) {
     return null;
+  }
+
+  if (user.email !== email && role !== ROLE_ADM) {
+    return notAuthorization();
   }
 
   const { deletedCount } = await remove(id);
