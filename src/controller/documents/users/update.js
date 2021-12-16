2625f09cd523e9a0c07e81da3392d127cabdd16f
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-16 00:20:18 -0300

    refatora controller users update.js

--- src/controller/documents/users/update.js
+++ src/controller/documents/users/update.js
@@ -1,8 +1,9 @@
 const { OK, NOT_FOUND } = require('http-status-codes').StatusCodes;
 
 const { update } = require('../../../service/documents/users');
-
 const { modifiedSuccessfully, notFound } = require('../../../service/utils/messages');
+
+const USER = 'user';
 
 module.exports = async (req, res, next) => {
   const { id } = req.params;
@@ -15,14 +16,14 @@ module.exports = async (req, res, next) => {
   if (!updated) {
     return next({
       status: NOT_FOUND,
-      message: notFound('users'),
+      message: notFound(USER),
     });
   }
 
   return res
     .status(OK)
     .json({
-      message: modifiedSuccessfully('user'),
+      message: modifiedSuccessfully(USER),
       updatedUser: updated,
     });
 };
