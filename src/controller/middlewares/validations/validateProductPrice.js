ebe9f7624711d22de1ffea9d1cdee14350b7d14b
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-17 13:44:13 -0300

    cria validateProductPrice.js

---
+++ src/controller/middlewares/validations/validateProductPrice.js
@@ -0,0 +1,35 @@
+const { BAD_REQUEST } = require('http-status-codes').StatusCodes;
+
+const { verifyNumbers } = require('../../../service/validations');
+
+const { required, notNumber } = require('../../../service/utils/messages');
+
+const PRICE = 'price';
+const DECIMAL_PLACES = 2;
+const MESSAGE_NOT_A_NUMBER = 'not a number';
+
+module.exports = async (req, _res, next) => {
+  const { price } = req.body;
+
+  const validation = verifyNumbers(price);
+
+  if (!validation) {
+    return next({
+      status: BAD_REQUEST,
+      message: required(PRICE),
+    });
+  }
+
+  if (validation === MESSAGE_NOT_A_NUMBER) {
+    return next({
+      status: BAD_REQUEST,
+      message: notNumber(PRICE),
+    });
+  }
+
+  const convertDecimalPlaces = price.toFixed(DECIMAL_PLACES);
+
+  req.body.price = convertDecimalPlaces;
+
+  return next();
+};
