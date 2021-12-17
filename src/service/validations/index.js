1f9c763c98a4c003f34d86849e5644317efd08b2
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-17 14:55:39 -0300

    corrige erro de digitacao no index do validations

--- src/service/validations/index.js
+++ src/service/validations/index.js
@@ -1,11 +1,11 @@
 const verifyEmail = require('./verifyEmail');
 const verifyRequeriment = require('./verifyRequeriment');
 const verifyNumbers = require('./verifyNumbers');
-const verityUnity = require('./verifyUnity');
+const verifyUnity = require('./verifyUnity');
 
 module.exports = {
   verifyEmail,
   verifyRequeriment,
   verifyNumbers,
-  verityUnity,
+  verifyUnity,
 };
