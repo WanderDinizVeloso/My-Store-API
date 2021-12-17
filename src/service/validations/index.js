5e70d2803c7538a51bfdf0153d2737bdcbf4a966
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-17 13:23:41 -0300

    implementa validateNumbers no index de validations

--- src/service/validations/index.js
+++ src/service/validations/index.js
@@ -1,7 +1,9 @@
 const verifyEmail = require('./verifyEmail');
 const verifyRequeriment = require('./verifyRequeriment');
+const verifyNumbers = require('./verifyNumbers');
 
 module.exports = {
   verifyEmail,
   verifyRequeriment,
+  verifyNumbers,
 };
