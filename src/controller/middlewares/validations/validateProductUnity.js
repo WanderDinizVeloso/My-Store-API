6c928ffd15b2d1b469fd96af8b7585d506b65a86
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-17 14:17:34 -0300

    cria validateProductUnity.js

---
+++ src/controller/middlewares/validations/validateProductUnity.js
@@ -0,0 +1,32 @@
+const { BAD_REQUEST } = require('http-status-codes').StatusCodes;
+
+const { verifyUnity } = require('../../../service/validations');
+
+const { required, notLength } = require('../../../service/utils/messages');
+
+const UNITY = 'unity';
+const INITIAL_LENGTH = 2;
+const FINAL_LENGTH = 3;
+const MESSAGE_NO_LENGTH = 'no length';
+
+module.exports = async (req, _res, next) => {
+  const { unity } = req.body;
+
+  const validation = verifyUnity(unity, INITIAL_LENGTH, FINAL_LENGTH);
+
+  if (!validation) {
+    return next({
+      status: BAD_REQUEST,
+      message: required(UNITY),
+    });
+  }
+
+  if (validation === MESSAGE_NO_LENGTH) {
+    return next({
+      status: BAD_REQUEST,
+      message: notLength(UNITY, INITIAL_LENGTH, FINAL_LENGTH),
+    });
+  }
+
+  return next();
+};
