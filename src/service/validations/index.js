e3e78fad6ff0a4bd932710caccc240dbfa734a81
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-17 14:19:00 -0300

    implementa verifyUnity no index da validations

--- src/service/validations/index.js
+++ src/service/validations/index.js
@@ -1,9 +1,11 @@
 const verifyEmail = require('./verifyEmail');
 const verifyRequeriment = require('./verifyRequeriment');
 const verifyNumbers = require('./verifyNumbers');
+const verityUnity = require('./verifyUnity');
 
 module.exports = {
   verifyEmail,
   verifyRequeriment,
   verifyNumbers,
+  verityUnity,
 };
