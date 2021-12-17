97c88faaa5d57ebccd78c024c15b8436d2b59bb9
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-16 23:27:15 -0300

    cria controller products update.js

---
+++ src/controller/documents/products/update.js
@@ -0,0 +1,29 @@
+const { OK, NOT_FOUND } = require('http-status-codes').StatusCodes;
+
+const { update } = require('../../../service/documents/products');
+const { modifiedSuccessfully, notFound } = require('../../../service/utils/messages');
+
+const PRODUCT = 'product';
+
+module.exports = async (req, res, next) => {
+  const { id } = req.params;
+  const { name, unity, quantity, category, price } = req.body;
+
+  const newUpdate = { id, name, unity, quantity, category, price };
+
+  const updated = await update(newUpdate);
+
+  if (!updated) {
+    return next({
+      status: NOT_FOUND,
+      message: notFound(PRODUCT),
+    });
+  }
+
+  return res
+    .status(OK)
+    .json({
+      message: modifiedSuccessfully(PRODUCT),
+      updatedUser: updated,
+    });
+};
