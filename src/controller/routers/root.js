6f26e30d760a66c3f05b21fbb016efdec1d16ad7
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-16 23:31:53 -0300

    cria e implementa router products

--- src/controller/routers/root.js
+++ src/controller/routers/root.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const users = require('./users');
 const login = require('./login');
+const products = require('./products');
 
 const root = express.Router({ mergeParams: true });
 
 root.use('/users', users);
 root.use('/login', login);
+root.use('/products', products);
 
 module.exports = root;
