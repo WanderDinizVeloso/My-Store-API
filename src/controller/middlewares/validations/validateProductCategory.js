d9592bf85f28ee669bef616845e0d53ce787dfa3
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-17 12:53:38 -0300

    cria validateProductCategory.js

---
+++ src/controller/middlewares/validations/validateProductCategory.js
@@ -0,0 +1,31 @@
+const { BAD_REQUEST } = require('http-status-codes').StatusCodes;
+
+const { verifyRequeriment } = require('../../../service/validations');
+
+const { required, notLength } = require('../../../service/utils/messages');
+
+const CATEGORY = 'category';
+const lENGTH = 4;
+const MESSAGE_NO_LENGTH = 'no length';
+
+module.exports = async (req, _res, next) => {
+  const { category } = req.body;
+
+  const validation = verifyRequeriment(category, lENGTH);
+
+  if (!validation) {
+    return next({
+      status: BAD_REQUEST,
+      message: required(CATEGORY),
+    });
+  }
+
+  if (validation === MESSAGE_NO_LENGTH) {
+    return next({
+      status: BAD_REQUEST,
+      message: notLength(CATEGORY, lENGTH),
+    });
+  }
+
+  return next();
+};
