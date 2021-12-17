eafb1e9cca9eb5775844eba1ed5e306966a79090
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-17 13:36:58 -0300

    cria validateProductQauntity.js

---
+++ src/controller/middlewares/validations/validateProductQuantity.js
@@ -0,0 +1,35 @@
+const { BAD_REQUEST } = require('http-status-codes').StatusCodes;
+
+const { verifyNumbers } = require('../../../service/validations');
+
+const { required, notNumber } = require('../../../service/utils/messages');
+
+const QUANTITY = 'quantity';
+const DECIMAL_PLACES = 3;
+const MESSAGE_NOT_A_NUMBER = 'not a number';
+
+module.exports = async (req, _res, next) => {
+  const { quantity } = req.body;
+
+  const validation = verifyNumbers(quantity);
+
+  if (!validation) {
+    return next({
+      status: BAD_REQUEST,
+      message: required(QUANTITY),
+    });
+  }
+
+  if (validation === MESSAGE_NOT_A_NUMBER) {
+    return next({
+      status: BAD_REQUEST,
+      message: notNumber(QUANTITY),
+    });
+  }
+
+  const convertDecimalPlaces = quantity.toFixed(DECIMAL_PLACES);
+
+  req.body.quantity = convertDecimalPlaces;
+
+  return next();
+};
