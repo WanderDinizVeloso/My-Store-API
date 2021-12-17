f7d7857293a1a02e0262b4118997cfca856fe9f1
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-16 23:06:37 -0300

    cria controller products remove.js

---
+++ src/controller/documents/products/remove.js
@@ -0,0 +1,26 @@
+const { OK, NOT_FOUND } = require('http-status-codes').StatusCodes;
+
+const { remove } = require('../../../service/documents/products');
+const { deletedSuccessfully, notFound } = require('../../../service/utils/messages');
+
+const PRODUCT = 'product';
+
+module.exports = async (req, res, next) => {
+  const { id } = req.params;
+  
+  const removed = await remove(id);
+
+  if (!removed) {
+    return next({
+      status: NOT_FOUND,
+      message: notFound(PRODUCT),
+    });
+  }
+
+  return res
+    .status(OK)
+    .json({
+      message: deletedSuccessfully(PRODUCT),
+      deletedUser: removed,
+    });
+};
