3236e0812173bd3a0bc95e65414867af032fd167
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-16 23:16:22 -0300

    cria controller products searchById.js

---
+++ src/controller/documents/products/searchById.js
@@ -0,0 +1,23 @@
+const { OK, NOT_FOUND } = require('http-status-codes').StatusCodes;
+
+const { searchById } = require('../../../service/documents/products');
+const { notFound } = require('../../../service/utils/messages');
+
+const PRODUCT = 'product';
+
+module.exports = async (req, res, next) => {
+  const { id } = req.params;
+
+  const product = await searchById(id);
+
+  if (!product) {
+    return next({
+      status: NOT_FOUND,
+      message: notFound(PRODUCT),
+    });
+  }
+
+  return res
+    .status(OK)
+    .json({ product });
+};
