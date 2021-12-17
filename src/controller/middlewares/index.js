5998a007012517941b7084794432a383526d200d
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-17 14:23:49 -0300

    implementa novas validacoes no index do middlewares

--- src/controller/middlewares/index.js
+++ src/controller/middlewares/index.js
@@ -5,6 +5,11 @@ const validateFirstName = require('./validations/validateFirstName');
 const validateLastName = require('./validations/validateLastName');
 const validateId = require('./validations/validateId');
 const validatePassword = require('./validations/validatePassword');
+const validateProductCategory = require('./validations/validateProductCategory');
+const validateProductName = require('./validations/validateProductName');
+const validateProductPrice = require('./validations/validateProductPrice');
+const validateProductQuantity = require('./validations/validateProductQuantity');
+const validateProductUnity = require('./validations/validateProductUnity');
 const authentication = require('./auth/authentication');
 const userAuthorization = require('./auth/userAuthorization');
 
@@ -16,6 +21,11 @@ module.exports = {
   validateLastName,
   validateId,
   validatePassword,
+  validateProductCategory,
+  validateProductName,
+  validateProductPrice,
+  validateProductQuantity,
+  validateProductUnity,
   authentication,
   userAuthorization,
 };
