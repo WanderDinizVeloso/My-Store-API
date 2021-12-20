98c5c07d15b907404c814931caccde2838f0fc30
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-20 12:01:32 -0300

    cria verificacao do type do parametro no validation verifyUnity.js

--- src/service/validations/verifyUnity.js
+++ src/service/validations/verifyUnity.js
@@ -1,8 +1,12 @@
-const { NO_LENGTH } = require('../utils/strings');
+const { NO_LENGTH, NOT_A_STRING } = require('../utils/strings');
 
 module.exports = (param, initialNumber, finalNumber) => {
-  if (!param || param === '') {
+  if (!param) {
     return null;
+  }
+
+  if (typeof param !== 'string') {
+    return NOT_A_STRING;
   }
 
   if (param.length > finalNumber || param.length < initialNumber) {
