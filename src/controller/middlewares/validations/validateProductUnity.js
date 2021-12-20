78c63c6f33a5650f760a45d96ea75c0edf40a6d1
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-20 12:32:57 -0300

    cria novo returno para validateProductUnity.js

--- src/controller/middlewares/validations/validateProductUnity.js
+++ src/controller/middlewares/validations/validateProductUnity.js
@@ -1,8 +1,8 @@
 const { BAD_REQUEST } = require('http-status-codes').StatusCodes;
 
 const { verifyUnity } = require('../../../service/validations');
-const { required, notBetweenTwoNumbers } = require('../../../service/utils/messages');
-const { UNITY, NO_LENGTH } = require('../../../service/utils/strings');
+const { required, notBetweenTwoNumbers, notString } = require('../../../service/utils/messages');
+const { UNITY, NO_LENGTH, NOT_A_STRING } = require('../../../service/utils/strings');
 
 const INITIAL_LENGTH = 2;
 const FINAL_LENGTH = 3;
@@ -16,6 +16,10 @@ const ERROR = {
     status: BAD_REQUEST,
     message: notBetweenTwoNumbers(UNITY, INITIAL_LENGTH, FINAL_LENGTH),
   },
+  BAD_REQUEST_NOT_STRING: {
+    status: BAD_REQUEST,
+    message: notString(UNITY),
+  },
 };
 
 module.exports = async (req, _res, next) => {
@@ -25,6 +29,7 @@ module.exports = async (req, _res, next) => {
 
   if (!validation) { return next(ERROR.BAD_REQUEST_REQUIRED); }
   if (validation === NO_LENGTH) { return next(ERROR.BAD_REQUEST_NOT_LENGTH); }
+  if (validation === NOT_A_STRING) { return next(ERROR.BAD_REQUEST_NOT_STRING); }
 
   const upperCase = unity.toUpperCase();
 
