8d9f92efc7f69fe2b6090f0c902aad8c597ea499
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-20 11:51:30 -0300

    cria verificacao do type do parametro no validation verifyRequreriment.js

--- src/service/validations/verifyRequeriment.js
+++ src/service/validations/verifyRequeriment.js
@@ -1,8 +1,12 @@
-const { NO_LENGTH } = require('../utils/strings');
+const { NO_LENGTH, NOT_A_STRING } = require('../utils/strings');
 
 module.exports = (param, length) => {
   if (!param || param === '') {
     return null;
+  }
+
+  if (typeof param !== 'string') {
+    return NOT_A_STRING;
   }
 
   if (param.length < length) {
