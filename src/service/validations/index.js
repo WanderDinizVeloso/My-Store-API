8520a4c43f489013ac67d62840621c5b5a8a5e9d
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-20 13:51:44 -0300

    cria validations verifyCaracters.js

--- src/service/validations/index.js
+++ src/service/validations/index.js
@@ -2,10 +2,12 @@ const verifyEmail = require('./verifyEmail');
 const verifyRequeriment = require('./verifyRequeriment');
 const verifyNumbers = require('./verifyNumbers');
 const verifyUnity = require('./verifyUnity');
+const verifyCaracters = require('./verifyCaracters');
 
 module.exports = {
   verifyEmail,
   verifyRequeriment,
   verifyNumbers,
   verifyUnity,
+  verifyCaracters,
 };
---
+++ src/service/validations/verifyCaracters.js
@@ -0,0 +1,13 @@
+const { INVALID_PASSWORD } = require('../utils/strings');
+
+module.exports = (password) => {
+  const upperCaseRegex = /[A-Z]+/.test(password);
+  const numberRegex = /[0-9]+/.test(password);
+  const specialCaracterRegex = /[!$#%_]+/.test(password);
+
+  if (!upperCaseRegex || !numberRegex || !specialCaracterRegex) {
+    return INVALID_PASSWORD;
+  }
+
+  return password;
+};
