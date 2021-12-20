f072d3a50237a665f468ab6591b2b77069288a43
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-20 12:48:00 -0300

    cria novo returno para validateEmail.js

--- src/controller/middlewares/validations/validateEmail.js
+++ src/controller/middlewares/validations/validateEmail.js
@@ -1,8 +1,8 @@
 const { BAD_REQUEST } = require('http-status-codes').StatusCodes;
 
 const { verifyEmail } = require('../../../service/validations');
-const { required, invalid } = require('../../../service/utils/messages');
-const { EMAIL, NOT_A_EMAIL } = require('../../../service/utils/strings');
+const { required, invalid, notString } = require('../../../service/utils/messages');
+const { EMAIL, NOT_A_EMAIL, NOT_A_STRING } = require('../../../service/utils/strings');
 
 const ERROR = {
   BAD_REQUEST_REQUIRED: {
@@ -13,6 +13,10 @@ const ERROR = {
     status: BAD_REQUEST,
     message: invalid(EMAIL),
   },
+  BAD_REQUEST_NOT_STRING: {
+    status: BAD_REQUEST,
+    message: notString(EMAIL),
+  },
 };
 
 module.exports = async (req, _res, next) => {
@@ -21,6 +25,7 @@ module.exports = async (req, _res, next) => {
   const validation = verifyEmail(email);
 
   if (!validation) { return next(ERROR.BAD_REQUEST_REQUIRED); }
+  if (validation === NOT_A_STRING) { return next(ERROR.BAD_REQUEST_NOT_STRING); }
   if (validation === NOT_A_EMAIL) { return next(ERROR.BAD_REQUEST_INVALID); }
 
   return next();
