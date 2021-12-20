b60b4ae1c835a362fe2cbc3615b00c7566d3bb9d
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-20 16:40:59 -0300

    implementa login de administrador

--- src/service/documents/login/login.js
+++ src/service/documents/login/login.js
@@ -3,18 +3,20 @@ const { compare } = require('bcrypt');
 const { USERS } = require('../../utils/strings');
 const { searchAll } = require('../../../model')(USERS);
 const { getToken } = require('../../auth');
+const { verifyAdm } = require('../../validations');
 
 module.exports = async ({ email, password }) => {
+  await verifyAdm(email, password);
   const allUsers = await searchAll();
-
+  
   const searchUser = allUsers.find((user) => user.email === email);
-
+  
   if (!allUsers.length || !searchUser) {
     return null;
   }
-
+  
   const matchPassword = await compare(password, searchUser.password);
-
+ 
   if (!matchPassword) {
     return null;
   }
