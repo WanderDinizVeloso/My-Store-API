9ea91de2cabdd7db7d9525d898b72bd1febc7c2d
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-20 12:54:21 -0300

    cria novo returno para verifyEmail.js

--- src/service/validations/verifyEmail.js
+++ src/service/validations/verifyEmail.js
@@ -1,10 +1,14 @@
-const { NOT_A_EMAIL } = require('../utils/strings');
+const { NOT_A_EMAIL, NOT_A_STRING } = require('../utils/strings');
 
 module.exports = (email) => {
   const emailVerificationRegex = /(\w+[@]\w+\.[a-z]{3,3}(\.[a-z]{2,2})?)$/.test(email);
 
   if (!email) {
     return null;
+  }
+
+  if (typeof email !== 'string') {
+    return NOT_A_STRING;
   }
 
   if (!emailVerificationRegex) {
