a4af9c7acb023f4605e3fd85c28f8f32cb7b2e5a
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-20 12:49:31 -0300

    refatora utils messages.js

--- src/service/utils/messages.js
+++ src/service/utils/messages.js
@@ -8,31 +8,31 @@ const notLength = (param, length) =>
   `The '${param}' field must contain at least ${length} characters`;
 
 const notBetweenTwoNumbers = (param, initialLength, finalLength) =>
-  `The ${param} field must contain between ${initialLength} and ${finalLength} characters.`;
+  `The '${param}' field must contain between ${initialLength} and ${finalLength} characters.`;
 
 const notNumber = (param) =>
-  `The ${param} field must be a number.`;
+  `The '${param}' field must be a number.`;
 
 const notString = (param) =>
-  `The ${param} field must be a string.`;
+  `The '${param}' field must be a string.`;
 
 const createdSuccessfully = (param) => 
-  `${param} created successfully.`;
+  `'${param}' created successfully.`;
 
 const deletedSuccessfully = (param) =>
-  `${param} deleted successfully,`;
+  `'${param}' deleted successfully,`;
 
 const modifiedSuccessfully = (param) =>
-  `${param} modified successfully.`;
+  `'${param}' modified successfully.`;
 
 const notFound = (param) =>
-  `${param} not found.`;
+  `'${param}' not found.`;
 
 const notRegistered = (param) =>
-  `no registered ${param}.`;
+  `no registered '${param}'.`;
 
 const registered = (param) =>
-  `${param} is already.`;
+  `'${param}' is already.`;
 
 const notAuthorization = () =>
   'Request not allowed for this user.';
