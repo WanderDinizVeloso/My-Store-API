3758ba1ca040adfdbcba2d28692bad86d2d1081e
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-21 09:25:12 -0300

    implementa middlewares authentication e admAuthorization na rota sales

--- src/controller/routers/sales.js
+++ src/controller/routers/sales.js
@@ -4,6 +4,8 @@ const {
   wrapper,
   validateSale,
   validateId,
+  authentication,
+  admAuthorization,
 } = require('../middlewares');
 
 const {
@@ -17,22 +19,29 @@ const {
 const router = express.Router({ mergeParams: true });
 
 router.get('/',
+  wrapper(authentication),
   wrapper(searchAll));
 
 router.get('/:id',
+  wrapper(authentication),
   wrapper(validateId),
   wrapper(searchById));
 
 router.post('/',
+  wrapper(authentication),
   wrapper(validateSale),
   wrapper(create));
 
 router.put('/:id',
+  wrapper(authentication),
+  wrapper(admAuthorization),
   wrapper(validateId),
   wrapper(validateSale),
   wrapper(update));
 
 router.delete('/:id',
+  wrapper(authentication),
+  wrapper(admAuthorization),
   wrapper(validateId),
   wrapper(remove));
 
