6db7ace4854d515bdd1859c7cdc4eddb096431e5
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-21 10:57:06 -0300

    refatora service verifySales.js

--- src/service/validations/verifySales.js
+++ src/service/validations/verifySales.js
@@ -1,67 +1,18 @@
-const verifyRequeriment = require('./verifyRequeriment');
-const verifyNumbers = require('./verifyNumbers');
-const verifyUnity = require('./verifyUnity');
+const verifySalesNumbers = require('./verifySalesNumbers');
+const verifySalesStrings = require('./verifySalesStrings');
 
-const LENGTH = 4;
-const INITIAL_LENGTH = 2;
-const FINAL_LENGTH = 3;
-const QUANTITY_DECIMAL_PLACES = 3;
-const PRICE_DECIMAL_PLACES = 2;
-
-const validationStrings = (sale) => {
-  const { name, category, unity } = sale;
-
-  const namecheck = verifyRequeriment(name, LENGTH);
-  const categoryCheck = verifyRequeriment(category, LENGTH);
-  const unityCheck = verifyUnity(unity, INITIAL_LENGTH, FINAL_LENGTH);
-
-  if (namecheck !== name || categoryCheck !== category || unityCheck !== unity) {
-    return null;
-  }
-
-  return { name, category, unity };
-};
-
-const validationNumbers = (sale) => {
-  const { quantity, price } = sale;
-
-  const quantityCheck = verifyNumbers(quantity);
-  const priceCheck = verifyNumbers(price);
-
-  if (quantityCheck !== quantity || priceCheck !== price) {
-    return null;
-  }
-
-  return { quantity, price };
-};
-
-const dataConvert = (sale) => {
-  const { unity, quantity, price } = sale;
-
-  const upperCase = unity.toUpperCase();
-  const quantityConvert = quantity.toFixed(QUANTITY_DECIMAL_PLACES);
-  const priceConvert = price.toFixed(PRICE_DECIMAL_PLACES);
-
-  const saleModified = {
-    ...sale,
-    unity: upperCase,
-    quantity: quantityConvert,
-    price: priceConvert,
-  };
-
-  return saleModified;
-};
+const { dataSalesConvert } = require('../functions');
 
 module.exports = (saleData) => saleData.reduce((acc, sale) => {
-  const strings = validationStrings(sale);
-  const numbers = validationNumbers(sale);
+  const strings = verifySalesStrings(sale);
+  const numbers = verifySalesNumbers(sale);
 
   if (!strings || !numbers) {
     acc.error = true;
     return acc;
   }
 
-  const convert = dataConvert(sale);
+  const convert = dataSalesConvert(sale);
 
   acc.products = [
     ...acc.products,
