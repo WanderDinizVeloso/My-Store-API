57b06e902f4bb067b895d011608c527052439a05
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-21 10:50:51 -0300

    cria verifySalesStrings.js

---
+++ src/service/validations/verifySalesStrings.js
@@ -0,0 +1,20 @@
+const verifyRequeriment = require('./verifyRequeriment');
+const verifyUnity = require('./verifyUnity');
+
+const LENGTH = 4;
+const INITIAL_LENGTH = 2;
+const FINAL_LENGTH = 3;
+
+module.exports = (sale) => {
+  const { name, category, unity } = sale;
+
+  const namecheck = verifyRequeriment(name, LENGTH);
+  const categoryCheck = verifyRequeriment(category, LENGTH);
+  const unityCheck = verifyUnity(unity, INITIAL_LENGTH, FINAL_LENGTH);
+
+  if (namecheck !== name || categoryCheck !== category || unityCheck !== unity) {
+    return null;
+  }
+
+  return { name, category, unity };
+};
