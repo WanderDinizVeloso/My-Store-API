98c9461c7ebffbf14ace282b7e9d6f5df02c9d6c
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-21 16:36:12 -0300

    refatora controller sales create.js

--- src/controller/documents/sales/create.js
+++ src/controller/documents/sales/create.js
@@ -1,28 +1,19 @@
-const { CREATED, BAD_REQUEST } = require('http-status-codes').StatusCodes;
+const { CREATED } = require('http-status-codes').StatusCodes;
 
 const { create } = require('../../../service/documents/sales');
-const { createdSuccessfully, registered } = require('../../../service/utils/messages');
+const { createdSuccessfully } = require('../../../service/utils/messages');
 const { SALE } = require('../../../service/utils/strings');
 
-const ERROR = {
-  BAD_REQUEST: {
-    status: BAD_REQUEST,
-    message: registered(SALE),
-  },
-};
-
-module.exports = async (req, res, next) => {
-  const newSale = req.body;
+module.exports = async (req, res, _next) => {
+  const sales = req.body;
   const { _id: userId } = req.user;
 
-  const created = await create({ newSale, userId });
-
-  if (!created) { return next(ERROR.BAD_REQUEST); }
+  const created = await create({ sales, userId });
 
   return res
     .status(CREATED)
     .json({
       message: createdSuccessfully(SALE),
-      createdProduct: created,
+      createdSale: created,
     });
 };
