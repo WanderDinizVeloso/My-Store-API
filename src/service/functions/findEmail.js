8370017ccae993bea966a4603be0f33a473f6e38
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-22 10:23:04 -0300

    refatora function findEmail.js

--- src/service/functions/findEmail.js
+++ src/service/functions/findEmail.js
@@ -1,7 +1,7 @@
-const { searchAll } = require('../documents/users');
+const searchAll = require('../documents/users/searchAll');
 
 module.exports = async (user) => {  
-  const allUsers = await searchAll() || [];
+  const allUsers = await searchAll() || [];  
   
   const verifyUser = allUsers.find(({ email }) => email === user.email);
 
