779ac0ce5d6dac002fa29d413a80726d0cf6a0b5
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-22 08:39:43 -0300

    implementa findEmail no index do service functions

--- src/service/functions/index.js
+++ src/service/functions/index.js
@@ -1,7 +1,9 @@
 const salesWithTotalAndAmount = require('./salesWithTotalAndAmount');
 const dataSalesConvert = require('./dataSalesConvert');
+const findEmail = require('./findEmail');
 
 module.exports = {
   salesWithTotalAndAmount,
   dataSalesConvert,
+  findEmail,
 };
