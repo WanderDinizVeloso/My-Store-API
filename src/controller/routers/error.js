6ea3265511fda79f846a3b379fdfdc8ce2610b56
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-23 12:16:57 -0300

    cria router error.js

---
+++ src/controller/routers/error.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const {
+  wrapper,
+  authentication,
+  admAuthorization,
+} = require('../middlewares');
+
+const { searchAll } = require('../documents/error');
+
+const router = express.Router({ mergeParams: true });
+
+router.get('/',
+  wrapper(authentication),
+  wrapper(admAuthorization),
+  wrapper(searchAll));
+  
+module.exports = router;
