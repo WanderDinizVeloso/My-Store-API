155803ce306bc1330bc0c6af334f96c776291dab
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-23 12:26:34 -0300

    refatora middleware error.js

--- src/controller/middlewares/error/error.js
+++ src/controller/middlewares/error/error.js
@@ -5,14 +5,14 @@ const { create } = require('../../../service/documents/error');
 
 module.exports = async (err, _req, res, _next) => {
   const { status = null, message } = err;
+
+  await create({ status: status || INTERNAL_SERVER_ERROR, message });
   
   if (status) {
     return res
       .status(status)
       .json({ error: { message } });
   }
-
-  await create({ status: status || INTERNAL_SERVER_ERROR, message });
 
   return res
     .status(INTERNAL_SERVER_ERROR)
