7fb150c267d3fbd111433ba176ba6f329656f97a
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-24 13:58:46 -0300

    refatora middleware error.js

--- src/controller/middlewares/error/error.js
+++ src/controller/middlewares/error/error.js
@@ -1,19 +1,26 @@
 const { INTERNAL_SERVER_ERROR } = require('http-status-codes').StatusCodes;
 
 const { internalError } = require('../../../service/utils/messages');
+const { UNAUTHENTICATED } = require('../../../service/utils/strings');
 const { create } = require('../../../service/documents/error');
 
-module.exports = async (err, _req, res, _next) => {
+module.exports = async (err, req, res, _next) => {
   const { status = null, message } = err;
-
-  await create({ status: status || INTERNAL_SERVER_ERROR, message });
+  
+  const {
+    originalUrl: URL,
+    body: { password, ...bodyWithoutPassword },
+    user = UNAUTHENTICATED,
+  } = req;
   
   if (status) {
     return res
-      .status(status)
-      .json({ error: { message } });
+    .status(status)
+    .json({ error: { message } });
   }
 
+  await create({ message, URL, bodyWithoutPassword, user });
+  
   return res
     .status(INTERNAL_SERVER_ERROR)
     .json({ error: { message: internalError() } });
