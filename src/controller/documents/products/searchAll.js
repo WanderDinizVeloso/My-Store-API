b5cd30ab59eeaf4010abafcb86c1dc282007f6b0
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-27 08:22:24 -0300

    refatora controller products searchAll.js

--- src/controller/documents/products/searchAll.js
+++ src/controller/documents/products/searchAll.js
@@ -1,20 +1,13 @@
-const { OK, NOT_FOUND } = require('http-status-codes').StatusCodes;
+const { OK } = require('http-status-codes').StatusCodes;
 
 const { searchAll } = require('../../../service/documents/products');
-const { notRegistered } = require('../../../service/utils/messages');
+const { notRegistered } = require('../../statusAndMessage');
 const { PRODUCTS } = require('../../../service/utils/strings');
-
-const ERROR = {
-  NOT_FOUND: {
-    status: NOT_FOUND,
-    message: notRegistered(PRODUCTS),
-  },
-};
 
 module.exports = async (_req, res, next) => {
   const products = await searchAll();
 
-  if (!products) { return next(ERROR.NOT_FOUND); }
+  if (!products) { return next(notRegistered(PRODUCTS)); }
 
   return res
     .status(OK)
