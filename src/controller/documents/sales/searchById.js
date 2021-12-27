9be115bf11a36d655813cf88f8d1c925ce5d941c
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-27 08:30:55 -0300

    refatora controller sales searchById.js

--- src/controller/documents/sales/searchById.js
+++ src/controller/documents/sales/searchById.js
@@ -1,22 +1,15 @@
-const { OK, NOT_FOUND } = require('http-status-codes').StatusCodes;
+const { OK } = require('http-status-codes').StatusCodes;
 
 const { searchById } = require('../../../service/documents/sales');
-const { notFound } = require('../../../service/utils/messages');
+const { notFound } = require('../../statusAndMessage');
 const { SALE } = require('../../../service/utils/strings');
-
-const ERROR = {
-  NOT_FOUND: {
-    status: NOT_FOUND,
-    message: notFound(SALE),
-  },
-};
 
 module.exports = async (req, res, next) => {
   const { id } = req.params;
 
   const sale = await searchById(id);
 
-  if (!sale) { return next(ERROR.NOT_FOUND); }
+  if (!sale) { return next(notFound(SALE)); }
 
   return res
     .status(OK)
