19c217e3b8e877136d21c8490f7fb96fd00f0cfe
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-12-27 08:36:34 -0300

    refatora controller users searchAll.js

--- src/controller/documents/users/searchAll.js
+++ src/controller/documents/users/searchAll.js
@@ -1,20 +1,13 @@
-const { OK, NOT_FOUND } = require('http-status-codes').StatusCodes;
+const { OK } = require('http-status-codes').StatusCodes;
 
 const { searchAll } = require('../../../service/documents/users');
-const { notRegistered } = require('../../../service/utils/messages');
+const { notRegistered } = require('../../statusAndMessage');
 const { USERS } = require('../../../service/utils/strings');
-
-const ERROR = {
-  NOT_FOUND: {
-    status: NOT_FOUND,
-    message: notRegistered(USERS),
-  },
-};
 
 module.exports = async (_req, res, next) => {
   const users = await searchAll();
 
-  if (!users) { return next(ERROR.NOT_FOUND); }
+  if (!users) { return next(notRegistered(USERS)); }
 
   return res
     .status(OK)
