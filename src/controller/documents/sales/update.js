1fee6e5a4b99cf86e77e4c1006a2ded270cc8099
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-01-03 12:23:22 -0300

    refatora controller sales update.js

--- src/controller/documents/sales/update.js
+++ src/controller/documents/sales/update.js
@@ -7,9 +7,9 @@ const { SALE } = require('../../../service/strings');
 module.exports = async (req, res, next) => {
   const { id } = req.params;
   const { _id: userId } = req.user;
-  const sale = req.body;
+  const saleData = req.body;
 
-  const updated = await update({ id, sale, userId });
+  const updated = await update({ id, saleData, userId });
 
   if (!updated) {
     return next(notFound(SALE));
