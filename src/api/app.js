1592e46eeac19c5eb6fd800889c32839c5db10a5
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-01-10 08:23:28 -0300

    refatora app.js

--- src/api/app.js
+++ src/api/app.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const { json } = require('body-parser');
 
-require('dotenv').config();
-
 const root = require('../controller/routers/root');
 const { error } = require('../controller/middlewares');
+
+require('dotenv').config();
 
 const app = express();
 
 app.use(json());
-
 app.use(root);
-
 app.use(error);
 
 module.exports = app;
