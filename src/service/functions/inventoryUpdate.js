4688283101f4d630091539a784dca730bf59097f
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-01-10 21:35:33 -0300

    refatora function inventoryUpdate, implementando Promise.all

--- src/service/functions/inventoryUpdate.js
+++ src/service/functions/inventoryUpdate.js
@@ -4,31 +4,30 @@ const { ADDITION, SUBTRACTION } = require('../strings');
 const QUANTITY_DECIMAL_PLACES = 3;
 const RADIX = 10;
 
-module.exports = async (productList, operator) => productList.soldProducts
-  .forEach((product) => {
-    const result = async () => {        
-      const { _id: id, quantity: saleQuantity } = product;
+module.exports = async (productList, operator) => {
+  const products = productList.map(({ _id: id }) => searchById(id));
+  const productsStock = await Promise.all(products);
+
+  const result = productList.map((productsSold, index) => {
+    const { quantity: saleQuantity } = productsSold;
+    const { _id: id, quantity: currentQuantity, ...productRest } = productsStock[index];
+    
+    let newQuantity = 0;
+
+    if (operator === ADDITION) {
+      newQuantity = parseInt(currentQuantity, RADIX) + parseInt(saleQuantity, RADIX);
+    } else if (operator === SUBTRACTION) {
+      newQuantity = parseInt(currentQuantity, RADIX) - parseInt(saleQuantity, RADIX);
+    }
   
-      const productData = await searchById(id);
-      
-      const { _id, quantity: currentQuantity, ...productDataWithoutIdAndQuantity } = productData;
+    const convertedQuantity = newQuantity.toFixed(QUANTITY_DECIMAL_PLACES);
 
-      let newQuantity = 0;
+    const updatedProductData = { id, ...productRest, quantity: convertedQuantity };
 
-      if (operator === ADDITION) {
-        newQuantity = parseInt(currentQuantity, RADIX) + parseInt(saleQuantity, RADIX);
-      } else if (operator === SUBTRACTION) {
-        newQuantity = parseInt(currentQuantity, RADIX) - parseInt(saleQuantity, RADIX);
-      }
-
-      const convertedQuantity = newQuantity.toFixed(QUANTITY_DECIMAL_PLACES);
-  
-      const updatedProductData = {
-        id, ...productDataWithoutIdAndQuantity, quantity: convertedQuantity,
-      };
-      
-      await update(updatedProductData);
-    };
-
-    result();
+    return update(updatedProductData);
   });
+
+  const resultResolve = await Promise.all(result);
+
+  return resultResolve;
+};
