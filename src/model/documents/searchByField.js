0e222a348bb031703d60193ec4e37d43291ea39f
WanderDinizVeloso <wanveloso@yahoo.com.br>
2022-01-10 21:30:04 -0300

    refatora model searchByField

--- src/model/documents/searchByField.js
+++ src/model/documents/searchByField.js
@@ -3,8 +3,7 @@ const connection = require('../connection');
 module.exports = async (collection, field) => {
   const entity = (await connection())
     .collection(collection)
-    .find(field)
-    .toArray();
+    .findOne(field);
 
   return entity;
 };
